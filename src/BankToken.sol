0bb14d5d2981639b9a31e836f820260a38d4cc76
FelipeLohan <felipelohan03052006@gmail.com>
2025-04-08 11:10:21 -0300

    First Commit, imported zeppelin library

---
+++ src/BankToken.sol
@@ -0,0 +1,20 @@
+// contracts/GLDToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BankToken is ERC20, Ownable {
+    constructor(address initialOwner, uint256 initialSupply) 
+      ERC20("BankToken", "BKT") 
+      Ownable(initialOwner) 
+    {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+}
